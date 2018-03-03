53839cd76f9a6eca94e2327c889a9316e88135d6
priyaA01 <pryshanthi.it@gmail.com>
2018-03-03 09:01:14 -0600

    db changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE burgers_db;
 
-	-- drop table burgers_db.burgers;
-
 CREATE TABLE burgers_db.burgers(
 		id INT AUTO_INCREMENT PRIMARY KEY,
         burger_name VARCHAR(100) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,1 @@
-INSERT INTO burgers_db.burgers (burger_name,devoured) VALUES ("Taco Burgers",false);
-INSERT INTO burgers_db.burgers (burger_name,devoured) VALUES ("Chicken Caesar Burger",false);
-INSERT INTO burgers_db.burgers (burger_name,devoured) VALUES ("Turkey Burger with a Twist",false);
-INSERT INTO burgers_db.burgers (burger_name,devoured) VALUES ("Mushroom-Stuffed Cheeseburgers",false);
+INSERT INTO burgers_db.burgers (burger_name) VALUES ("Taco Burgers"),("Chicken Caesar Burger"),("Turkey Burger with a Twist"),("Mushroom-Stuffed Cheeseburgers");
